a0e48aae603af9bfefb299d18e63e5a4feadacef
Brienna Herold <brienna.herold@gmail.com>
2016-10-22 20:38:03 -0400

    Change usage of ArrayList to StorageResource and add comments

--- 2. Java programming - solving problems with software/Week 4/3. Programming exercise - analyzing baby names/AnalyzeBabyNames.java
+++ 2. Java programming - solving problems with software/Week 4/3. Programming exercise - analyzing baby names/AnalyzeBabyNames.java
@@ -1,8 +1,15 @@
 import edu.duke.*;
 import org.apache.commons.csv.*;
-import java.util.ArrayList;
 /**
- * Write a description of AnalyzeBabyNames here.
+ * totalBirths() prints the number of unique boy names, the number of unique girl names, the total
+ * number of names, total number of births, total number of girl births, and total number of boy 
+ * births in the file. 
+ * 
+ * getRank() returns the rank of the name in the file for the given gender, where rank 1 is the 
+ * name with the largest number of births. If the name is not in the file, then -1 is returned.
+ * @param year is an int representing the year of the file to parse
+ * @param name is a String representing the name to calculate a rank for
+ * @param gender is a String representing the gender the name needs to be
  * 
  * @author Brienna Herold
  * @version Oct. 21, 2016
@@ -10,42 +17,41 @@ import java.util.ArrayList;
 public class AnalyzeBabyNames {
     public int getRank(int year, String name, String gender) {
         FileResource fr = new FileResource("data/us_babynames_by_year/yob" + year + ".csv");
-        CSVParser parser = fr.getCSVParser(false);
-        
+
         int births = 0;
-        // Loop through every row in the file
-        for (CSVRecord rec : parser) {
-            // Retrieve number of births if name AND gender match
+        // For every name in the file
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            // Retrieve and convert number of births if name AND gender match
             if (rec.get(0).equals(name) && rec.get(1).equals(gender)) {
-                System.out.println(rec);
                 births = Integer.parseInt(rec.get(2));
                 break;
             }
         }
         
         int rank = 1;
-        parser = fr.getCSVParser(false);
-        ArrayList<Integer> ranks = new ArrayList<Integer>();
-        // Loop through every row again
-        for (CSVRecord row : parser) {
-            int currentNumOfBirths = Integer.parseInt(row.get(2));
-            // If same gender AND current number of births is greater than that retrieved
-            if (row.get(1).equals(gender) && currentNumOfBirths > births) {
-                // Increment rank by 1 if number of births is unique
-                // cuz every rank may have more than 1 record with the same number of births
-                // and we don't want to rank down for those duplicates
-                if (!ranks.contains(currentNumOfBirths)) {
+        StorageResource nums = new StorageResource();
+        // For every name in the file
+        for (CSVRecord record : fr.getCSVParser(false)) {
+            // Retrieve current number of births (leaving as String so it's addable to StorageResource)
+            String currentNumOfBirths = record.get(2);
+            // Convert current number of births so it's useable in numeric comparison
+            int currentNumOfBirthsInt = Integer.parseInt(currentNumOfBirths);
+            // If gender matches argument AND current number of births is greater than that retrieved
+            if (record.get(1).equals(gender) && currentNumOfBirthsInt > births) {
+                // Increment rank only if the number of births is not found in nums
+                // cuz a rank may encompass several names with the same number of births
+                if (!nums.contains(currentNumOfBirths)) {
                     rank++;
                 }
-                ranks.add(currentNumOfBirths);
+                nums.add(currentNumOfBirths);
             }
         } 
         
+        // If no name met given conditions, return -1, otherwise return rank
         if (births == 0) {
             return -1;
-        } else {
-            return rank;
-        }
+        } 
+        return rank;
     }
     
     public void testGetRank() {
@@ -56,31 +62,45 @@ public class AnalyzeBabyNames {
     }
 
     public void totalBirths () {
+        // Allow user to choose file
         FileResource fr = new FileResource();
-        
+        // Set variables to initial values
         int totalBirths = 0;
         int totalBoys = 0;
         int totalGirls = 0;
         int totalNames = 0;
-        ArrayList<String> uniqueGirlNames = new ArrayList<String>();
-        ArrayList<String> uniqueBoyNames = new ArrayList<String>();
-        
+        // Create empty lists to hold unique girl and boy names
+        StorageResource uniqueGirlNames = new StorageResource();
+        StorageResource uniqueBoyNames = new StorageResource();
+        // For every name
         for (CSVRecord rec : fr.getCSVParser(false)) {
+            // Retrieve number of births for that name
             int numBorn = Integer.parseInt(rec.get(2));
+            // Add number of births to total number of births
             totalBirths += numBorn;
+            // Increment total count of names
             totalNames++;
+            // Retrieve name
+            String name = rec.get(0);
+            // If gender is male
             if (rec.get(1).equals("M")) {
+                // Add number of births to total number of births for males
                 totalBoys += numBorn;
-                String name = rec.get(0);
-                uniqueBoyNames.add(name);
+                // Add name to list of boy names if not already on list
+                if (!uniqueBoyNames.contains(name)) {
+                    uniqueBoyNames.add(name);
+                }
             }
+            // Otherwise (if gender is female)
             else {
+                // Add number of births to total number of births for females
                 totalGirls += numBorn;
-                String name = rec.get(0);
-                uniqueGirlNames.add(name);
+                // Add name to list of girl names if not already on list
+                if (!uniqueGirlNames.contains(name)) {
+                    uniqueGirlNames.add(name);
+                }
             }
         }
-        
         System.out.println("Unique boy names: " + uniqueBoyNames.size());
         System.out.println("Unique girl names: " + uniqueGirlNames.size());
         System.out.println("Total names: " + totalNames);
