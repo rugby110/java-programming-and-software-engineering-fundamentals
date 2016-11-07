c7fccd001098a8bbfc7874cbadacc9c9e733390e
Brienna Herold <brienna.herold@gmail.com>
2016-11-06 23:00:35 -0500

    Complete assignment 2

---
+++ 3. Java programming - arrays, lists, and structured data/Week 1/7. Programming exercise - object oriented Caesar cipher/CaesarCipherTwo.java
@@ -0,0 +1,70 @@
+
+/**
+ * Encrypts and decrypts a String using the Caesar algorithm with two keys.
+ * @parameter key1 is an int representing the alphabet shift used to encrypt every other character,
+ * starting with the first character
+ * @parameter key2 is an int representing the alphabet shift used to encrypt every other character, 
+ * starting with the second character
+ * 
+ * @author Brienna Herold
+ * @version Nov. 6, 2016
+ */
+public class CaesarCipherTwo {
+    private String alphabet;
+    private String shiftedAlphabet1; 
+    private String shiftedAlphabet2;
+    private int mainKey1;
+    private int mainKey2;
+    
+    public CaesarCipherTwo(int key1, int key2) {
+        alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        shiftedAlphabet1 = alphabet.substring(key1) + alphabet.substring(0, key1);
+        shiftedAlphabet2 = alphabet.substring(key2) + alphabet.substring(0, key2);
+        mainKey1 = key1;
+        mainKey2 = key2;
+    }
+    
+    /**
+     * Returns a String that is the input encrypted using the two shifted alphabets.
+     */
+    public String encrypt(String input) {
+        // Make passed String dynamically mutable
+        StringBuilder encrypted = new StringBuilder(input);
+        // For each character
+        for (int i = 0; i < encrypted.length(); i++) {
+            // Get character
+            char ch = encrypted.charAt(i);
+            // If char is a letter
+            if (Character.isLetter(ch)) {
+                // Convert to uppercase (regardless if it is already)
+                char chUpper = Character.toUpperCase(ch);
+                // Find its position in normal abc
+                int pos = alphabet.indexOf(chUpper);
+                // Find it in one of the encrypted alphabets, depending on index 
+                char replacee;
+                if (i % 2 == 0) {
+                    replacee = shiftedAlphabet1.charAt(pos);
+                } else {
+                    replacee = shiftedAlphabet2.charAt(pos);
+                }
+                // Replace it with letter found in encrypted abc (in correct case)
+                if (chUpper == ch) {
+                    encrypted.setCharAt(i, replacee);
+                } else {
+                    encrypted.setCharAt(i, Character.toLowerCase(replacee));
+                }
+            }
+        }
+        
+        return encrypted.toString();
+    }
+    
+    /**
+     * Returns a String that is decrypted using the same keys as in its encryption.
+     */
+    public String decrypt(String input) {
+        CaesarCipherTwo cc2 = new CaesarCipherTwo(26 - mainKey1, 26 - mainKey2);
+        String decrypted = cc2.encrypt(input);
+        return decrypted;
+    }
+}
---
+++ 3. Java programming - arrays, lists, and structured data/Week 1/7. Programming exercise - object oriented Caesar cipher/TestCaesarCipherTwo.java
@@ -0,0 +1,106 @@
+import edu.duke.*;
+/**
+ * @author Brienna Herold
+ * @version Nov. 6, 2016
+ */
+public class TestCaesarCipherTwo {
+    private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    /**
+     * Creates an array of letter frequencies in parameter String s 
+     * and returns the index of the largest letter frequency.
+     */
+    public int countLetters(String s) {
+        int[] freqs = new int[26];
+        // For every character in the string
+        for (int i = 0; i < s.length(); i++) {
+            // Extract character
+            char ch = s.charAt(i);
+            // If character is a letter
+            if (Character.isLetter(ch)) {
+                // Identify its position in the alphabet
+                int index = alphabet.indexOf(Character.toUpperCase(ch));
+                // Use this position to update its freq in the freqs array 
+                freqs[index]++;
+            }
+        }
+        // Return the index of the largest letter frequency
+        return maxIndex(freqs);
+    }
+    
+    /**
+     * Calculates the index of the largest letter frequency from an array of all letter frequencies.
+     * @parameter freqs is an array containing letter frequencies, each index representing 
+     * the corresponding index in the alphabet (e.g. 0 represents the 0th letter, a)
+     * @returns the index as an int
+     */
+    public int maxIndex(int[] values) {
+        // Set max to the first index
+        int max = 0;
+        // For every freq after the first freq,
+        for (int i = 1; i < values.length; i++) {
+            // If freq is larger than freq at max index, set max index to current index
+            if (values[max] < values[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    
+    /**
+     * Returns a new String that is every other character from message 
+     * starting with the start position.
+     */
+    public String halfOfString(String message, int start) {
+        String halved = "";
+        for (int i = start; i < message.length(); i += 2) {
+            halved += message.charAt(i);
+        }
+        return halved;
+    }
+    
+    /**
+     * Returns the key to later decrypt parameter String s after calculating 
+     * the shift between the most common letter in s, assumed to be E, and E.
+     */
+    private int getKey(String s) {
+        // Calculate the index of the encrypted letter in String s that is most likely to be E
+        int maxIndex = countLetters(s);
+        // Calculate the shift between indexes of encrypted letter and E in the alphabet
+        int key = maxIndex - alphabet.indexOf('E');
+        // Return key
+        if (key > 0) {
+            return key;
+        } else {
+            return 26 + key;
+        }
+    }
+    
+    public void breakCaesarCipherTwo(String input) {
+        String halved1 = halfOfString(input, 0);
+        String halved2 = halfOfString(input, 1);
+        int key1 = getKey(halved1);
+        int key2 = getKey(halved2);
+        
+        CaesarCipherTwo cc2 = new CaesarCipherTwo(key1, key2);
+        String decrypted = cc2.decrypt(input);
+        
+        System.out.println("The decrypted message: ");
+        System.out.println(decrypted);
+    }
+    
+    public void simpleTests() {
+        FileResource fr = new FileResource();
+        CaesarCipherTwo cc2 = new CaesarCipherTwo(17, 3);
+        String encryptedFile = cc2.encrypt(fr.asString());
+        System.out.println("The encrypted file: ");
+        System.out.println(encryptedFile);
+        
+        // Decrypt file using the same keys it was encrypted with
+        String decryptedFile = cc2.decrypt(encryptedFile);
+        System.out.println("The decrypted file: ");
+        System.out.println(decryptedFile);
+        
+        // Decrypt file by determining the keys it was encrypted with
+        breakCaesarCipherTwo(encryptedFile);
+    }
+}
