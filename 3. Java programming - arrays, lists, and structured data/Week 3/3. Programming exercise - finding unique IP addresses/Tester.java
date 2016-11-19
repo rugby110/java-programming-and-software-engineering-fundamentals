86e0a9b9003c1d37cc73c7c6440332c46985bdbd
Brienna Herold <brienna.herold@gmail.com>
2016-11-19 14:09:56 -0500

    Complete exercise

--- 3. Java programming - arrays, lists, and structured data/Week 3/3. Programming exercise - finding unique IP addresses/Tester.java
+++ 3. Java programming - arrays, lists, and structured data/Week 3/3. Programming exercise - finding unique IP addresses/Tester.java
@@ -27,4 +27,52 @@ public class Tester
         // Print all the web logs
         analyzer.printAll();
     }
+    
+    public void testUniqueIPs() {
+        // Create a LogAnalyzer object
+        LogAnalyzer analyzer = new LogAnalyzer();
+        // Read the data file short-test_log
+        analyzer.readFile("short-test_log");
+        // Test the method countUniqueIPs
+        int numOfUniqueIPs = analyzer.countUniqueIPs();
+        System.out.println("Unique IPs: " + numOfUniqueIPs);
+    }
+    
+    public void testPrintAllHigherThanNum() {
+        // Create a LogAnalyzer object
+        LogAnalyzer analyzer = new LogAnalyzer();
+        // Read the data file
+        analyzer.readFile("weblog1_log");
+        // Test the method printAllHigherThanNum
+        int num = 400;
+        analyzer.printAllHigherThanNum(num);
+    }
+    
+    public void testUniqueIPVistsOnDay() {
+        // Create a LogAnalyzer object
+        LogAnalyzer analyzer = new LogAnalyzer();
+        // Read the data file weblog-short_log
+        analyzer.readFile("weblog1_log");
+        // Test the method uniqueIPVisitsOnDay
+        String someday = "Mar 24";
+        ArrayList<String> addresses = analyzer.uniqueIPVisitsOnDay(someday);
+        System.out.println("There were " + addresses.size() + " visits on " + someday);
+    }
+    
+    public void testUniqueIPsInRange() {
+        // Create a LogAnalyzer object
+        LogAnalyzer analyzer = new LogAnalyzer();
+        // Read the data file short-test_log
+        analyzer.readFile("weblog1_log");
+        // Test the method countUniqueIPsInRange
+        int low = 200;
+        int high = 299;
+        int num = analyzer.countUniqueIPsInRange(low, high);
+        System.out.println("Number of unique IPs in range " + low + "-" + high + ": " + num);
+        
+        low = 300;
+        high = 399;
+        num = analyzer.countUniqueIPsInRange(low, high);
+        System.out.println("Number of unique IPs in range " + low + "-" + high + ": " + num);
+    }
 }
