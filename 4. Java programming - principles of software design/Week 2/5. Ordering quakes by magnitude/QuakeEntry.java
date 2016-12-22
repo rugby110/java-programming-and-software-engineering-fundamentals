19a76613a1871d0f5ab929e0b797539383519674
Brienna Herold <brienna.herold@gmail.com>
2016-12-22 15:54:06 -0600

    Add Week 2 material

---
+++ 4. Java programming - principles of software design/Week 2/5. Ordering quakes by magnitude/QuakeEntry.java
@@ -0,0 +1,44 @@
+
+public class QuakeEntry implements Comparable<QuakeEntry>{
+	
+	private Location myLocation;
+	private String title;
+	private double depth;
+	private double magnitude;
+
+	public QuakeEntry(double lat, double lon, double mag, 
+	                  String t, double d) {
+		myLocation = new Location(lat,lon);
+		
+		magnitude = mag;
+		title = t;
+		depth = d;
+	}
+	
+	public Location getLocation(){
+		return myLocation;
+	}
+	
+	public double getMagnitude(){
+		return magnitude;
+	}
+	
+	public String getInfo(){
+		return title;
+	}
+	
+	public double getDepth(){
+		return depth;
+	}
+
+	@Override
+	public int compareTo(QuakeEntry loc) {
+	    return Double.compare(magnitude,loc.getMagnitude());
+
+	}
+	
+	public String toString(){
+		return String.format("(%3.2f, %3.2f), mag = %3.2f, depth = %3.2f, title = %s", myLocation.getLatitude(),myLocation.getLongitude(),magnitude,depth,title);
+	}
+	
+}
